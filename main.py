5e2cfd4771d2a31c103b3ae7af308b414756727c
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-11-09 22:31:27 -0500

    base file

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
 import ply.lex as lex
 
 reserved = {'if': 'IF', 'else': 'ELSE', 'while': 'WHILE', 'for': 'FOR'}
 
-# List of token names.   This is always required
 tokens = (
    'INT',
    'DECIMAL',
@@ -27,7 +20,6 @@ tokens = (
    'PRINT',
 ) + tuple(reserved.values())
 
-# Regular expression rules for simple tokens
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
@@ -39,8 +31,7 @@ t_RBRACKET = r'\]'
 t_MOD = r'%'
 t_SEMICOLON = r';'
 t_COLON = r','
-
-# A regular expression rule with some action code
+t_ignore  = ' \t'
 
 def t_PRINT(t):
     r'print'
@@ -61,33 +52,27 @@ def t_VAR(t):
     t.type = reserved.get(t.value, 'VAR')
     return t
 
-# Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
 
-# Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Build the lexer
+### 
+
 lexer = lex.lex()
 
-# Test it out
 data = '''
 5.2 ; print(hola) (1, 2, 3) []
 '''
 
-# Give the lexer some input
 lexer.input(data)
 
-# Tokenize
 while True:
     tok = lexer.token()
     if not tok:
-        break      # No more input
+        break
     print(tok)
