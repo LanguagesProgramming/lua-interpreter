efa84fc183e532da2c9684ab1681aae72eb0b663
Ariel-Vargas <arielvargas2004@hotmail.com>
2024-11-16 08:54:47 -0500

    Commit nuevo

--- Num_Par.lua
+++ Num_Par.lua
@@ -7,4 +7,14 @@ function esPar(n)
     return par
 end
     
-    
+num = 25
+num2 = 150
+num3 = 78
+num4 = 51
+num5 = 40
+
+local array = { 25, 150, 78, 51, 40 }
+
+for i = 1, #array do
+    print("El numero es "array[i])
+end
