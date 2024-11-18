df93d7a2852252d5efb011b8772d0fa6693e0d40
Ariel-Vargas <arielvargas2004@hotmail.com>
2024-11-18 18:53:48 -0500

    Ejemplo de log 2

---
+++ lua/contador.lua
@@ -0,0 +1,5 @@
+local i = 1
+while i <= 5 do
+    print("Contador: " .. i)
+    i = i + 1
+end
--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from lua_interpreter.parser import parser
 from lua_interpreter.lexer import lexer
 
 
-lua_code = '''while 1 do
-    repeat
-        x = 1
-    until x > 10
+lua_code = '''local function bubble_sort(arr)
+    local n = #arr
+    for i = 1, n - 1 do
+        for j = 1, n - i do
+            if arr[j] > arr[j + 1] then
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            end
+        end
+    end
+end
+
+local array = { 5, 4, 3, 2, 1 }
+bubble_sort(array)
+
+for i = 1, #array do
+    print(array[i])
 end
 '''
 
