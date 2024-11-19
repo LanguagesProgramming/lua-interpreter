a2b5715a2f79f075ccf6ccf80a5df5c589173e05
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-11-18 21:08:55 -0500

    test working on sort.lua

--- lua/sort.lua
+++ lua/sort.lua
@@ -1,4 +1,4 @@
-local function bubble_sort(arr) ; local n = #arr ; for i = 1, n - 1 do ; for j = 1, n - i do ; if arr[j] > arr[j + 1] then arr[j], arr[j + 1] = arr[j + 1], arr[j] end end end end
+local function bubble_sort(arr) local n = #arr ; for i = 1, n - 1 do for j = 1, n - i do if arr[j] > arr[j + 1] then arr[j], arr[j + 1] = arr[j + 1], arr[j] end end end end
 
 local array = { 5, 4, 3, 2, 1 }
 
